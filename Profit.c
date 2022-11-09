672f5f66b0c13b881e1d0c5cfd030661a240d44d
sankar pmbs <103130655+20A91A0385@users.noreply.github.com>
2022-11-09 14:14:47 +0530

    #include<stdio.h> int main() {     int a,b;     scanf("%d%d",&a,&b);     if(a<b+150)     {         printf("Profit");     }     else     {         printf("Loss");     }
    }

---
+++ Profit.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    if(a<b+150)
+    {
+        printf("Profit");
+    }
+    else
+    {
+        printf("Loss");
+    }
+    
+}
